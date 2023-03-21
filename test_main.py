0a3b21326f5f34b662aaf627a90b7258e6058d8f
Elena Ladygina <Ladygina_ev@yahoo.com>
2023-03-21 18:03:06 +0700

    fixed test

--- test_main.py
+++ test_main.py
@@ -1,4 +1,5 @@
 import random
+from selenium.common import ElementNotInteractableException
 from seleniumwire import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -8,6 +9,7 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 import pytest
+import requests
 
 link = "https://piter-online.net/"
 
@@ -15,8 +17,11 @@ link = "https://piter-online.net/"
 @pytest.fixture()
 def browser():
     options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
+    options.add_argument("--window-size=1920,1080")
     # options.add_argument('--headless')
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option("useAutomationExtension", False)
+    options.add_argument("--disable-blink-features=AutomationControlled")
     browser = webdriver.Chrome(
         service=ChromeService(ChromeDriverManager().install()), options=options
     )
@@ -26,99 +31,104 @@ def browser():
     browser.quit()
 
 
-# TestData
-street = "Тестовая линия"
-house_number = 1
-phone_number = 1111111111
-username = "Автотест"
-
-# Locators:
-INPUT_STREET = (
-    By.XPATH,
-    "(//span[contains(text(),'Введите улицу')])[1]/following-sibling::input",
-)
-INPUT_HOUSE = (By.XPATH, "(//span[contains(text(),'Дом')])[1]/following-sibling::input")
-# DROP_DOWN = (By.CSS_SELECTOR, ".app115 .icon-arrow-1-down")
-TYPE_CONNECT = (By.XPATH, "(//li[contains(text(),'В квартиру')])[1]")
-BTN_SHOW_TARIFF = (By.XPATH, '(//div[@data-test="find_tohome_button"])[1]')
-BTN_CONNECT = (
-    By.CSS_SELECTOR,
-    "[datatest='providers_form_inspect_connect_tariff_button']",
-)
-BTN_CLOSE_POPUP = (By.CSS_SELECTOR, "[datatest='close_popup1_from_quiz_input_tel']")
-INPUT_USERNAME = (By.CSS_SELECTOR, "[datatest='providers_provider_order_input_name']")
-INPUT_PHONE = (By.CSS_SELECTOR, "[datatest='providers_provider_order_input_tel']")
-SEND_BTN = (By.CSS_SELECTOR, "[data-test='order_form_input_connect_button']")
-LOGO = (By.CSS_SELECTOR, ".app10 a[aria-label]")
+class Data:
+    street = "Тестовая линия"
+    house_number = 1
+    phone_number = 1111111111
+    username = "Автотест"
 
 
-def test_user_can_make_request(browser):
-    """1. Открыть сайт https://piter-online.net/, в поиске ввести улицу Тестовая линия, дом 1"""
-    input_street = browser.find_element(*INPUT_STREET)
-    input_street.send_keys(street)
-    actions = ActionChains(browser)
-    actions.pause(5)
-    actions.move_to_element_with_offset(
-        to_element=input_street, xoffset=0, yoffset=-60
-    ).click()
-    actions.perform()
-    input_street.send_keys(Keys.ENTER)
-
-    input_house = WebDriverWait(browser, 10).until(
-        ec.element_to_be_clickable(INPUT_HOUSE)
+class Locators:
+    INPUT_STREET = (
+        By.XPATH,
+        "(//span[contains(text(),'Введите улицу')])[1]/following-sibling::input",
     )
-    input_house.send_keys(house_number)
-    actions.pause(2)
-    actions.move_to_element_with_offset(
-        to_element=input_house, xoffset=0, yoffset=-60
-    ).click()
-    actions.pause(3)
-    actions.perform()
-    input_house.send_keys(Keys.ENTER)
-
-    # drop_down = browser.find_element(*DROP_DOWN)
-    # drop_down.click()
-    select_type = browser.find_element(*TYPE_CONNECT)
-    select_type.click()
-
-    btn_show_tariff = browser.find_element(*BTN_SHOW_TARIFF)
-    btn_show_tariff.click()
-
-    """2.На открывшейся странице кликнуть на "подключить" или "подключить по акции" на тарифе"""
-    close_popup = browser.find_element(*BTN_CLOSE_POPUP)
-    close_popup.click()
-    all_btn_connect = list(browser.find_elements(*BTN_CONNECT))
-    random_index = random.randrange(len(all_btn_connect))
-    btn_connect = all_btn_connect[random_index]
-    btn_connect.click()
-
-    """"3. На открывшейся странице ввести имя Автотест, номер телефона 1111111111."""
-    input_username = (
-        WebDriverWait(browser, 10)
-        .until(ec.visibility_of_element_located(INPUT_USERNAME))
-        .send_keys(username)
+    INPUT_HOUSE = (
+        By.XPATH,
+        "(//span[contains(text(),'Дом')])[1]/following-sibling::input",
     )
-    input_phone = browser.find_element(*INPUT_PHONE)
-    input_phone.send_keys(phone_number)
-    # api_url = browser.current_url
-    send_request = browser.find_element(*SEND_BTN)
-    send_request.click()
-
-    """5. Убедиться, что у всех отправленных заявок статус 200."""
-
-    request_last = browser.last_request
-    print(request_last.date)
-
-    # assert request_last.response.date == 200, "Wrong status code"
-
-    logo = browser.find_element(*LOGO)
-    logo.click()
-    assert f"{browser.current_url}" == f"{link}leningradskaya-oblast"
-
-
-def test_repit_4_times(browser):
-    """Повторить пункты 2-3 пять раз."""
-    for i in range(4):
-        test_user_can_make_request(browser)
+    DROP_DOWN = (By.CSS_SELECTOR, ".app115 .icon-arrow-1-down")
+    TYPE_CONNECT = (By.XPATH, "(//li[contains(text(),'В квартиру')])[1]")
+    BTN_SHOW_TARIFF = (By.XPATH, '(//div[@data-test="find_tohome_button"])[1]')
+    BTN_CONNECT = (
+        By.CSS_SELECTOR,
+        "[datatest='providers_form_inspect_connect_tariff_button']",
+    )
+    BTN_CLOSE_POPUP = (By.CSS_SELECTOR, "[datatest='close_popup1_from_quiz_input_tel']")
+    INPUT_USERNAME = (
+        By.CSS_SELECTOR,
+        "[datatest='providers_provider_order_input_name']",
+    )
+    INPUT_PHONE = (By.CSS_SELECTOR, "[datatest='providers_provider_order_input_tel']")
+    SEND_BTN = (By.CSS_SELECTOR, "[data-test='order_form_input_connect_button']")
+    LOGO = (By.CSS_SELECTOR, ".app10 a[aria-label]")
 
 
+class TestCase:
+    def test_user_can_make_request(self, browser):
+        """1. Открыть сайт https://piter-online.net/, в поиске ввести улицу Тестовая линия, дом 1"""
+        input_street = browser.find_element(*Locators.INPUT_STREET)
+        input_street.send_keys(Data.street)
+        actions = ActionChains(browser)
+        actions.pause(3)
+        actions.move_to_element_with_offset(
+            to_element=input_street, xoffset=0, yoffset=-60
+        ).click()
+        actions.perform()
+        actions.pause(3)
+        input_street.send_keys(Keys.ENTER)
+
+        wait = WebDriverWait(browser, 10)
+        input_house = wait.until(ec.element_to_be_clickable(Locators.INPUT_HOUSE))
+        input_house.send_keys(Data.house_number)
+        actions.move_to_element_with_offset(
+            to_element=input_house, xoffset=0, yoffset=-60
+        ).click()
+        actions.perform()
+        input_house.send_keys(Keys.ENTER)
+
+        try:
+            select_type = wait.until(ec.element_to_be_clickable(Locators.TYPE_CONNECT))
+            select_type.click()
+        except ElementNotInteractableException:
+            drop_down = browser.find_element(*Locators.DROP_DOWN)
+            drop_down.click()
+            select_type = browser.find_element(*Locators.TYPE_CONNECT)
+            browser.execute_script("arguments[0].click();", select_type)
+
+        btn_show_tariff = browser.find_element(*Locators.BTN_SHOW_TARIFF)
+        btn_show_tariff.click()
+
+        """2.На открывшейся странице кликнуть на "подключить" или "подключить по акции" на тарифе"""
+        close_popup = browser.find_element(*Locators.BTN_CLOSE_POPUP)
+        close_popup.click()
+        all_btn_connect = list(browser.find_elements(*Locators.BTN_CONNECT))
+        random_index = random.randrange(len(all_btn_connect))
+        btn_connect = all_btn_connect[random_index]
+        btn_connect.click()
+
+        """"3. На открывшейся странице ввести имя Автотест, номер телефона 1111111111."""
+        wait.until(ec.visibility_of_element_located(Locators.INPUT_USERNAME)).send_keys(
+            Data.username
+        )
+        input_phone = browser.find_element(*Locators.INPUT_PHONE)
+        input_phone.send_keys(Data.phone_number)
+
+        send_request_btn = browser.find_element(*Locators.SEND_BTN)
+        try:
+            actions.scroll_to_element(send_request_btn).click().perform()
+        except ElementNotInteractableException:
+            browser.execute_script("return arguments[0].scrollIntoView(true);", send_request_btn)
+            browser.execute_script("arguments[0].click();", send_request_btn)
+
+        """5. Убедиться, что у всех отправленных заявок статус 200."""
+        # for request in browser.requests:
+        #     if request.response:
+        #         assert request.response.status_code == 200, "Wrong status code"
+        response = requests.get(f"{browser.current_url}")
+        assert response.status_code == 200, "Wrong status code"
+
+    def test_repit_4_times(self, browser):
+        """Повторить пункты 2-3 пять раз."""
+        for i in range(4):
+            self.test_user_can_make_request(browser)
